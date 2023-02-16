4662b64f24b261229becb5c522f4b1ddb128f34e
Andrei Kudrautsau <kavalerand@gmail.com>
2023-02-16 23:08:18 +0300

    added task10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int result = (number/10) - (number / 100) * 10;
+// Console.WriteLine($"В числе {number} вторая цифра => {result}");
+
+int secondDigital = SecondDigital (number);
+Console.WriteLine($"В числе {number} вторая цифра => {secondDigital}");
+
+int SecondDigital (int num)
+{
+    int digitalSecond = (num/10) - (num / 100) * 10;
+    return digitalSecond;
+}
