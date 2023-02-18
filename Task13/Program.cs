ba20e283c55a69170cf8c1185210452c6396af81
Andrei Kudrautsau <kavalerand@gmail.com>
2023-02-18 23:08:18 +0300

    delete mistakes

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,13 +6,12 @@
 Console.WriteLine("Введите любое число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if ( Math.Abs (number) > 0 && Math.Abs (number) < 99) Console.WriteLine ($"В числе {number} НЕТ третьей цифры");
-if ( Math.Abs (number) > 99 && Math.Abs (number) < 999) Console.WriteLine ($"В числе {number} третья цифра => {number % 10}");
-else if ( Math.Abs (number) > 999) 
-{    
-    while (Math.Abs (number) > 999)
+if (Math.Abs(number) > 0 && Math.Abs(number) < 99) Console.WriteLine($"В числе {number} НЕТ третьей цифры");
+if (Math.Abs(number) > 99 && Math.Abs(number) < 999) Console.WriteLine($"В числе {number} третья цифра => {number % 10}");
+if (Math.Abs(number) > 999) Console.WriteLine($"В числе {number} третья цифра => {Division(number) % 10}");
+
+int Division(int num)
 {
-    number = number / 10;    
-}
-Console.WriteLine ($"В числе {number} третья цифра => {number % 10}");
+    while (Math.Abs(num) > 999) num = num / 10;
+    return num % 10;
 }
